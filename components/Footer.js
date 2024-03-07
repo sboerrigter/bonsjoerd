47b02442d9868e302e36b127114651ad882aa84e
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2024-03-07 20:11:23 +0100

    :construction: Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,40 +1,61 @@
 import Button from "@/components/Button";
+import Socials from "./Socials";
 import Wrap from "@/components/Wrap";
 
 export default function Footer() {
   return (
     <footer className="bg-green-700 text-white -mt-1 pt-1" id="contact">
       <Wrap>
-        <div id="contact" className="bg-green-600 -mt-15 p-10 rounded-xl">
-          <h2 className="text-[50px] leading-tight mb-6 xl:text-[60px]">
+        <div
+          id="contact"
+          className="bg-green-600 -mt-15 p-5 pb-10 2xs:p-10 2xs:pb-15 xl:p-15 xl:pt-10 rounded-xl"
+        >
+          <h2 className="text-[50px] leading-tight mb-6 xl:text-[60px] ">
             Get in touch
           </h2>
-          <p>
-            Don't hesitate to contact me if you need help with branding, design
-            or web development. I am happy to help where I can!
-          </p>
-          <Button className="mb-7.5" href="mailto:sjoerd@bonsjoerd.fr">
-            Send me an e-mail
-          </Button>
-          <p>
-            <strong>Sjoerd Boerrigter</strong>
-            <br />
-            13 Côte de Ravat
-            <br />
-            24200 Sarlat-la-Canéda
-            <br />
-            La France
-          </p>
-          <p>Siret: 98494519600017</p>
-          <p>
-            <a className="text-white" href="mailto:sjoerd@bonsjoerd.fr">
-              sjoerd@bonsjoerd.fr
-            </a>
-            <br />
-            <a className="text-white" href="tel:+31646277318">
-              +31 646 277 318
-            </a>
-          </p>
+
+          <div className="flex flex-col lg:flex-row gap-x-10 xl:gap-x-15 gap-y-10">
+            <div>
+              <p>
+                Don't hesitate to contact me if you need help with branding,
+                design or web development. I am happy to help where I can!
+              </p>
+              <Button href="mailto:sjoerd@bonsjoerd.fr">
+                <span>
+                  Send <span className="hidden xs:inline">me</span> an e-mail
+                </span>
+              </Button>
+            </div>
+
+            <div className="lg:w-[540px] flex-shrink-0 flex flex-col sm:flex-row gap-x-10 xl:gap-x-15 gap-y-7.5">
+              <div className="sm:w-1/2">
+                <p>
+                  <strong>Sjoerd Boerrigter</strong>
+                  <br />
+                  13 Côte de Ravat
+                  <br />
+                  24200 Sarlat-la-Canéda
+                  <br />
+                  La France
+                </p>
+                <p>Siret: 98494519600017</p>
+              </div>
+
+              <div className="sm:w-1/2">
+                <p>
+                  <a className="text-white" href="mailto:sjoerd@bonsjoerd.fr">
+                    sjoerd@bonsjoerd.fr
+                  </a>
+                  <br />
+                  <a className="text-white" href="tel:+31646277318">
+                    +31 646 277 318
+                  </a>
+                </p>
+
+                <Socials />
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="text-center py-8">
---
+++ components/Socials.js
@@ -0,0 +1,87 @@
+export default function Socials() {
+  const links = [
+    {
+      id: "github",
+      url: "https://github.com/sboerrigter",
+      icon: (
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke-width="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+        </svg>
+      ),
+    },
+    {
+      id: "linkedin",
+      url: "https://www.linkedin.com/in/sboerrigter/",
+      icon: (
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke-width="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+          <rect x="2" y="9" width="4" height="12"></rect>
+          <circle cx="4" cy="4" r="2"></circle>
+        </svg>
+      ),
+    },
+    {
+      id: "facebook",
+      url: "https://www.facebook.com/sjoerdboerrigter",
+      icon: (
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke-width="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+        </svg>
+      ),
+    },
+    {
+      id: "youtube",
+      url: "https://www.youtube.com/@bonsjoerd1",
+      icon: (
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke-width="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path>
+          <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon>
+        </svg>
+      ),
+    },
+  ];
+
+  return (
+    <div className="flex gap-3">
+      {links.map((link) => (
+        <a
+          key={link.id}
+          url={link.url}
+          target="_blank"
+          rel="nofollow"
+          className="w-12 h-12 flex items-center justify-center rounded-full text-gray-800 bg-white hover:bg-yellow-500"
+        >
+          {link.icon}
+        </a>
+      ))}
+    </div>
+  );
+}

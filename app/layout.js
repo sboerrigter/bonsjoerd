0bbf32f7b720e7a6fc1047d9b47f8a9a1a5d01f9
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2024-03-07 22:01:50 +0100

    :truck: Move metadata to page

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,6 @@ import { Poppins } from "next/font/google";
 
 const poppins = Poppins({ weight: ["400", "600", "700"], subsets: ["latin"] });
 
-export const metadata = {
-  title: "BonSjoerd - Branding, design & web development",
-  description:
-    "Sjoerd Boerrigter, freelance designer & web developer from the Netherlands, living in Sarlat-la-Canéda in the south of France.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="antialiased scroll-smooth">
--- app/page.js
+++ app/page.js
@@ -3,6 +3,12 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 
+export const metadata = {
+  title: "BonSjoerd - Branding, design & web development",
+  description:
+    "Sjoerd Boerrigter, freelance designer & web developer from the Netherlands, living in Sarlat-la-Canéda in the south of France.",
+};
+
 export default function Home() {
   return (
     <>

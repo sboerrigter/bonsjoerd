eff887b3c07d677a240be176d8a009d6e4c565aa
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2024-03-07 23:00:05 +0100

    :bug: Minor notices

--- components/Button.js
+++ components/Button.js
@@ -21,7 +21,7 @@ export default function Button({ children, className, href }) {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          stroke-linecap="square"
+          strokeLinecap="square"
           d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
         />
       </svg>
--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -22,15 +22,11 @@ export default function LanguageSwitcher() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="4"
+          strokeWidth="4"
           stroke="currentColor"
-          class="w-3 h-3"
+          className="w-3 h-3"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m19.5 8.25-7.5 7.5-7.5-7.5"
-          />
+          <path d="m19.5 8.25-7.5 7.5-7.5-7.5" />
         </svg>
       </div>
 

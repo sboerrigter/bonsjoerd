7ed85b5d12bff927b70cd69b9e71754461c4780c
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2024-03-08 20:11:48 +0100

    :construction: Project images

--- components/Projects.js
+++ components/Projects.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Wrap from "@/components/Wrap";
 
 export default function Projects() {
@@ -6,104 +7,123 @@ export default function Projects() {
       title: "Maison 1870",
       services: "Branding, design & web development",
       link: "https://maison1870.com/",
+      image: "/maison-1870.jpg",
     },
     {
       title: "Keizers tegels & sanitair",
       services: "Webdesign & development",
       link: "https://www.keizerssanitair.nl/",
+      image: "/keizers.jpg",
+    },
+    {
+      title: "iPhoned",
+      services: "Agile web development",
+      link: "https://www.iphoned.nl/",
+      image: "/iphoned.jpg",
     },
     {
       title: "Actief Twente",
       services: "Branding, webdesign & development",
       link: "https://www.actieftwente.nl/",
-    },
-    {
-      title: "Android Planet",
-      services: "Agile web development",
-      link: "https://www.androidplanet.nl/",
+      image: "/actief-twente.jpg",
     },
     {
       title: "The Flavor of Happiness",
       services: "Branding",
-      link: "/theflavorofhappiness.pdf",
+      link: "/the-flavor-of-happiness.pdf",
+      image: "/the-flavor-of-happiness.jpg",
+    },
+    {
+      title: "Naris",
+      services: "UX/UI Design",
+      link: "/naris.pdf",
+      image: "/naris.jpg",
     },
     {
       title: "DartsKing",
       services: "Agile web development",
       link: "https://dartsking.nl/",
-    },
-
-    {
-      title: "Naris",
-      services: "UX/UI Design",
-      link: "/naris.pdf",
-    },
-    {
-      title: "Ecommerce News",
-      services: "Agile web development",
-      link: "https://ecommercenews.eu/",
+      image: "/dartsking.jpg",
     },
     {
       title: "Streaming like a boss",
       services: "Branding, webdesign & development",
       link: "https://www.streaminglikeaboss.nl/",
-    },
-    {
-      title: "Dutch Shape",
-      services: "Web development",
-      link: "https://www.dutch-shape.nl/",
-    },
-    {
-      title: "B&O Leselschade",
-      services: "Branding, webdesign & development",
-      link: "https://www.juristenkantoor.nl/",
-    },
-    {
-      title: "Menkehorst Kwekerijën",
-      services: "Webdesign & development",
-      link: "https://www.menkehorst.nl/",
-    },
-    {
-      title: "Z-wave",
-      services: "Web development",
-      link: "https://www.z-wave.com/",
-    },
-    {
-      title: "SW Vastgoedverbetering",
-      services: "Web development",
-      link: "https://www.swbv.nl/",
-    },
-    {
-      title: "VDM Cars",
-      services: "Web development",
-      link: "https://www.vdmcars.de/",
+      image: "/streaming-like-a-boss.jpg",
     },
     {
       title: "ESS Easy Drain",
       services: "Webdesign & development",
       link: "https://www.easydrain.com/",
+      image: "/ess.jpg",
+    },
+    {
+      title: "Ecommerce News",
+      services: "Agile web development",
+      link: "https://ecommercenews.eu/",
+      image: "/ecommerce-news.jpg",
+    },
+    {
+      title: "Menkehorst Kwekerijën",
+      services: "Webdesign & development",
+      link: "https://www.menkehorst.nl/",
+      image: "/menkehorst.jpg",
+    },
+    {
+      title: "Dutch Shape",
+      services: "Web development",
+      link: "https://www.dutch-shape.nl/",
+      image: "/dutch-shape.jpg",
     },
     {
       title: "Velda",
       services: "Webdesign & development",
       link: "https://www.velda.com/",
+      image: "/velda.jpg",
+    },
+    {
+      title: "B&O Leselschade",
+      services: "Branding, webdesign & development",
+      link: "https://www.juristenkantoor.nl/",
+      image: "/bo-letselschade.jpg",
     },
     {
       title: "Quality bookings",
       services: "Webdesign & development",
       link: "https://www.quality-bookings.nl/",
+      image: "/quality-bookings.jpg",
     },
-    // @todo add this project when redesign is live
+
+    {
+      title: "SW Vastgoedverbetering",
+      services: "Web development",
+      link: "https://www.swbv.nl/",
+      image: "/sw.jpg",
+    },
+    {
+      title: "VDM Cars",
+      services: "Web development",
+      link: "https://www.vdmcars.de/",
+      image: "/vdm-cars.jpg",
+    },
+    {
+      title: "Z-wave",
+      services: "Web development",
+      link: "https://www.z-wave.com/",
+      image: "/z-wave.jpg",
+    },
+    // @todo add these projects when they are live
     // {
     //   title: "Smartphone.nl",
     //   services: "Agile web development",
     //   link: "https://www.smartphone.nl/",
+    // image: "/smartphone.jpg",
     // },
-    // @todo add this project when redesign is live
     // {
     //   title: "SKIM",
     //   services: "Webdesign & development",
     //   link: "https://www.skimgroup.com/",
+    // image: "/skimgroup.jpg",
     // },
   ];
 
@@ -147,13 +167,19 @@ export default function Projects() {
             href={project.link}
             key={project.title}
           >
-            <div className="aspect-square p-10 shadow-lg bg-white rounded-lg mb-5 group-hover:shadow-xl transition-shadow">
-              <div className="aspect-square flex items-center justify-center p-5 font-normal italic text-gray-600">
-                I'll add an image here
-                <br /> when I have time... 😅
+            <div className="p-5 shadow-lg bg-white rounded-lg mb-5 group-hover:shadow-xl transition-shadow duration-300">
+              <div className="overflow-hidden w-full">
+                <Image
+                  src={project.image}
+                  className="group-hover:scale-105 transition-transform duration-300 easy-in-out w-full aspect-square"
+                  alt={project.title}
+                  width={240}
+                  height={240}
+                  sizes="(max-width: 639px) 100vw, (max-width: 1023px) 50vw, (max-width: 1200px) 33vw, 340px"
+                />
               </div>
             </div>
-            <h3 className="font-semibold text-lg group-hover:text-green-500 flex justify-center items-center">
+            <h3 className="font-semibold text-lg flex justify-center items-center">
               {project.title}
 
               <span className="inline-block w-0 h-auto overflow-visible opacity-0 group-hover:opacity-100 group-hover:translate-x-2 transition-all">

16d298f84923d1f28639e1602d110fc0995cdf51
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2024-03-08 22:54:03 +0100

    :bug: Images are not sowing

--- middleware.js
+++ middleware.js
@@ -28,5 +28,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|img/|favicon.ico).*)", "/"],
+  matcher: ["/((?!api|_next/static|_next/image|public/|favicon.ico).*)", "/"],
 };
